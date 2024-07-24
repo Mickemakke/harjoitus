df6003243c058838e92f1792eaa79168bb572e6a
Mickemakke <miikasilvennoinen@gmail.com>
2024-07-24 23:22:24 +0300

    Create testi.py

---
+++ testi.py
@@ -0,0 +1,7 @@
+echo "# harjoitus" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Mickemakke/harjoitus.git
+git push -u origin main
